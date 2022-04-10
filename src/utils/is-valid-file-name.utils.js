122e6c79a8a7ccfcc1e00f21435700573694d4a3
vorozco-asistensi <vorozco.dev@gmail.com>
2022-04-10 12:35:38 -0500

    [ADD] Added OpenAPI Documentation

--- src/utils/is-valid-file-name.utils.js
+++ src/utils/is-valid-file-name.utils.js
@@ -1,5 +1,5 @@
 
-const fileNameValid = (text) => /^([a-zA-Z0-9]{1,}(.csv))$/.test(text);
+const fileNameValid = (text) => /^([a-zA-Z0-9]{1,}[.](csv))$/.test(text);
 
 export default fileNameValid
 
